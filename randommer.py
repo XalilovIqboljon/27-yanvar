fa59dbd59154e019b63f8ff25e5eecadf9a278d9
Xalilov <eljjey@gmail.com>
2021-01-27 18:58:37 +0500

    yangi randommer

--- randommer.py
+++ randommer.py
@@ -2,5 +2,9 @@ import requests
 
 headers={'X-Api-Key':'f6ee3c0e1e3148469afadf1793e2fb35'}
 url='https://randommer.io/api/Card'
-r=requests.get(url,headers=headers)
+payload={
+    "type":"Visa"
+}
+r=requests.get(url,params=payload, headers=headers)
 print(r.url)
+print(r.json())
