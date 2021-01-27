6f565cd4999248210ccc68e4ee6670bac6bec4b5
Xalilov <eljjey@gmail.com>
2021-01-27 18:59:07 +0500

    yangi dars

--- dars.py
+++ dars.py
@@ -3,10 +3,11 @@ import requests
 url = 'https://randomuser.me/api/'
 p={'results':5}
 r=requests.get(url,p)
+print(r.status_code)
 data=r.json()
 dic={}
 for i in data:
     nat=data['results'][0]['nat']
-    name=data['results'][0]['name']['first']+' '+data['results'][0]['name']['last']
+    name=data['results'][0]['name']['first']+'_'+data['results'][0]['name']['last']
     dic.get[name]=nat
     print(dic)
