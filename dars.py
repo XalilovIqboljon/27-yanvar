36356a75227f4d686f66cc0ab29bbc1e3345ca20
Xalilov <eljjey@gmail.com>
2021-01-27 17:59:25 +0500

    name+nat

--- dars.py
+++ dars.py
@@ -1,9 +1,12 @@
 import requests
 
 url = 'https://randomuser.me/api/'
-p={'results':2}
+p={'results':5}
 r=requests.get(url,p)
 data=r.json()
-nat=data['results'][0]['nat']
-# print(r.url)
-print(nat)
+dic={}
+for i in data:
+    nat=data['results'][0]['nat']
+    name=data['results'][0]['name']['first']+' '+data['results'][0]['name']['last']
+    dic.get[name]=nat
+    print(dic)
